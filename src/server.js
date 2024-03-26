f7bd75ae64666f84e53925b0ea6b8f45d7f9c550
Caroline Recla <carolinekhatlynrecla@gmail.com>
2024-03-26 16:51:39 -0300

    fix: vercel

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ require("./database/index.js");
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT | 3333, "0.0.0.0", () => {
   console.log("Servidor funcionando!");
 });
