913f2b3d7927b77657e7de60ab0c901d4baf5562
Caroline Recla <carolinekhatlynrecla@gmail.com>
2024-03-28 14:47:18 -0300

    fix return

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -69,6 +69,7 @@ class authController {
   }
 
   static async loginEmployee(req, res) {
+    return res.status(400).json({ msg: "Código do funcionário obrigatório." });
     try {
       const { employeeCode, password } = req.body;
 
